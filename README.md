11fc3ad837ba633a079f66e6f26c78549029bf38
Thyala Dias <dias.thyala@gmail.com>
2022-07-03 17:53:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto desenvolvido para uma academia ficticia. 
+Fou usado framworks, javascript, css e html.
---
+++ script/academia.js
@@ -0,0 +1,18 @@
+function msg() {
+    $("#mensagem").addClass('ver');
+    setTimeout(function () { $("#mensagem").removeClass('ver'); }, 2000);
+};
+
+$('footer-coluna li a').click(function(event) {
+  event.preventDefault();
+});
+
+
+var counter = 1;
+setInterval(function () {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
